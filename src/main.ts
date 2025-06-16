1af9e9844b086e715e4cf5a850cb9495da014830
Wendy Novelo <wendynovelo764@gmail.com>
2025-06-16 15:37:16 -0600

    cambiosssssssss

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Mi API')
+    .setDescription('Documentación API con Swagger')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
